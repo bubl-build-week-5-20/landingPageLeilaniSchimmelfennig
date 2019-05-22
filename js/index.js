02d1f62cc2c20d40bdced97e24520b777c8208dc
lschimm <lschimmelfennig@gmail.com>
2019-05-22 11:36:38 -0500

    finished objects! whew.

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ const siteContent = {
     },
     "main-content": {
         "about-h2":"About Bubl",
-        "about-content": "==Making friends can be difficult. Throw in a new school with a new enviroment and it can seem even harder. Bubl seeks to assist elementary children transitioning to middle school. With Bubl, social bubbles can begin to grow before classes even begin.",
-        "layout-h2":"==Kid Friendly Layout",
-        "layout-content": "==With our simple layout, kids will be able to navigate the app with ease. With less time spent figuring out the layout, children can quickly begin to make connections.",
+        "about-content": "Making friends can be difficult. Throw in a new school with a new enviroment and it can seem even harder. Bubl seeks to assist elementary children transitioning to middle school. With Bubl, social bubbles can begin to grow before classes even begin.",
+        "layout-h2":"Kid Friendly Layout",
+        "layout-content": "With our simple layout, kids will be able to navigate the app with ease. With less time spent figuring out the layout, children can quickly begin to make connections.",
         "controls-h2":"Parental Controls",
-        "controls-content": "==In the age of rising technology and limitless social emdia, many parents worry their children may have access to worrisome content. Find out more about our parental controls and safety features.",
+        "controls-content": "In the age of rising technology and limitless social emdia, many parents worry their children may have access to worrisome content. Find out more about our parental controls and safety features.",
     },
     "mid": {
     "button": "Sign Up",
@@ -25,18 +25,20 @@ const siteContent = {
 const button = document.querySelector('button')
 button.textContent = (siteContent["mid"]["button"])
 
+const main1 = document.querySelectorAll("middle, middle1, h2")
+main1[0].textContent = (siteContent["main-content"]["about-h2"])
+main1[1].textContent = (siteContent["main-content"]["layout-h2"])
+main1[2].textContent = (siteContent["main-content"]["controls-h2"])
+
+const main2 = document.querySelectorAll("middle, middle1, p")
+main2[1].textContent = (siteContent["main-content"]["about-content"])
+main2[2].textContent = (siteContent["main-content"]["layout-content"])
+main2[3].textContent = (siteContent["main-content"]["controls-content"])
+
 const foot = document.querySelector("container, footer")
 foot.textContent = (siteContent["footer"]["copyright"])
 
-const main1 = document.querySelectorAll("h2")
-main1[0].textContent = (siteContent["main-content"]["about-h2"])
-main1[1].textContent = (siteContent["main-content"]["about-content"])
-// main1[2].textContent = (siteContent["main-content"]["layout-h2"])
-// main1[3].textContent = (siteContent["main-content"]["layout-content"])
-// main1[4].textContent = (siteContent["main-content"]["controls-h2"])
-// main1[5].textContent = (siteContent["main-content"]["controls-content"])
 /*
-
 class Dropdown {
   constructor(element) {
     this.element = element;
